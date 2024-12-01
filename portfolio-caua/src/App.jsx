fdd78fc51ef2967bd0f1b191f0c66c0d1bb22692
Cauã Andrade <caua.goldenboyy@gmail.com>
2024-12-01 00:58:03 -0300

    almost finished

--- portfolio-caua/src/App.jsx
+++ portfolio-caua/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Typical from 'react-typical'
-
+import cr7 from '../src/assets/images/cr7.webp'
 function App() {
 
   
@@ -21,20 +21,26 @@ function App() {
         <section className="myself">
             <Typical className='animacao'
                     steps={[
-                      'Cauã Andrade Condé', 2000,
-                      "Dev-fullstack", 10000500,
+                      '<Cauã Andrade Condé/>', 2000,
+                      "Dev-fullstack", 10500,
                     ]}
                     loop={2} 
             /> 
 
-            <img src="" alt="Foto Cauã" />
+            <img src={cr7} alt="Foto Cauã" />
         </section>
         <section className="aboutme">
             <div className="cabecalho-sobremim">
-               Quem sou eu?
+              <h1>Quem sou eu?</h1>
             </div>
             <div className="content-sobremim">
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, quia laudantium! Vitae unde cum quaerat eos adipisci dolores quos reprehenderit. Inventore vitae expedita nisi vel id maiores mollitia, eligendi eaque.</p>
+
+              <div className="icones">
+                  <img src="" alt="Linkedin" />
+                  <img src="" alt="Currículo" />
+                  <img src="" alt="Github" />
+              </div>      
             </div>
         </section>
         <section className="stacks">
@@ -49,12 +55,35 @@ function App() {
             </div>
             <div className="cards-tecnologias">
                 <img src="" alt="foto javascript" />
-                <h4>Javascript</h4>
+                <h4>React</h4>
             </div>
             <div className="cards-tecnologias">
                 <img src="" alt="foto javascript" />
-                <h4>Javascript</h4>
+                <h4>Html</h4>
             </div>
+            <div className="cards-tecnologias">
+                <img src="" alt="foto javascript" />
+                <h4>CSSt</h4>
+            </div>
+            <div className="cards-tecnologias">
+                <img src="" alt="foto javascript" />
+                <h4>SCSS</h4>
+            </div>
+            <div className="cards-tecnologias">
+                <img src="" alt="foto javascript" />
+                <h4>MYSQL</h4>
+            </div>
+            <div className="cards-tecnologias">
+                <img src="" alt="foto javascript" />
+                <h4>Sql Server</h4>
+            </div>
+            <div className="cards-tecnologias">
+                <img src="" alt="foto javascript" />
+                <h4>Git</h4>
+            </div>
+        </section>
+        <section className="meus-projetos">
+
         </section>
       </div>     
   </main>
