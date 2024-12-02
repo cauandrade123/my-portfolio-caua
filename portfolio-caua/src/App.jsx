0850e223e8a0f86512a3a79a7f21d4a33c91a7d4
Cauã Andrade <caua.goldenboyy@gmail.com>
2024-12-02 16:06:05 -0300

    some repairs

--- portfolio-caua/src/App.jsx
+++ portfolio-caua/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="content">
         <header>
             <div className="caua">
-                <h1>Cauã Andrade  Condé</h1>
+                <h3> &lt;Cauã Andrade  Condé/&gt;</h3>
             </div>
             <div className="lista">
                 <li>Sobre Mim</li>
@@ -30,12 +30,12 @@ function App() {
 
                 <div className="links-midias">
                     <div className="botao-sobremim">
-                        <button className='butao-sobremim'>Sobremim</button>
+                        <button  className='butao-sobremim'>Baixar Currículo</button>
                     </div>
                     <div className="botoes">
                       <img className='icones' src="../src/assets/images/linkedin.png" alt="" />
                       <img className='icones' src="../src/assets/images/github.png" alt="" />
-                      <img className='icones' src="../src/assets/images/resume.png" alt="" />
+                      <img className='icones' src="../src/assets/images/whatsapp.png" alt="" />
                     </div>
                 </div>
             </div>
@@ -50,51 +50,51 @@ function App() {
             <div className="content-sobremim">
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, quia laudantium! Vitae unde cum quaerat eos adipisci dolores quos reprehenderit. Inventore vitae expedita nisi vel id maiores mollitia, eligendi eaque.</p>
 
-              <div className="icones">
-                  <img src="" alt="Linkedin" />
-                  <img src="" alt="Currículo" />
-                  <img src="" alt="Github" />
+              <div className="icon-web-dev">
+                  <img className='icon-webdev' src={cr7} alt="icon-web-dev" />
               </div>      
             </div>
         </section>
         <section className="stacks">
-            <h1>Tecnologias dominadas</h1>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto javascript" />
-                <h4>Javascript</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto Node" />
-                <h4>Node</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto javascript" />
-                <h4>React</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto html" />
-                <h4>Html</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto css" />
-                <h4>CSS</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto sass" />
-                <h4>SCSS</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto mysql" />
-                <h4>MYSQL</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto sql server" />
-                <h4>Sql Server</h4>
-            </div>
-            <div className="cards-tecnologias">
-                <img src="" alt="foto git" />
-                <h4>Git</h4>
-            </div>
+             <h1>Tecnologias dominadas</h1>
+                        <div className="cartoes">
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto javascript" />
+                                <h4>Javascript</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto Node" />
+                                <h4>Node</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto javascript" />
+                                <h4>React</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto html" />
+                                <h4>Html</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto css" />
+                                <h4>CSS</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto sass" />
+                                <h4>SCSS</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto mysql" />
+                                <h4>MYSQL</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto sql server" />
+                                <h4>Sql Server</h4>
+                            </div>
+                            <div className="cards-tecnologias">
+                                <img className='image-icon-techs' src="" alt="foto git" />
+                                <h4>Git</h4>
+                            </div>
+                        </div>      
         </section>
         <section className="meus-projetos">
             <div className="cabecalho-meusprojetos">
